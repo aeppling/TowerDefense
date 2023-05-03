b52294943bfd9424bbe5e61a18feb5799a9b2099
aeppling <adrien.eppling@epitech.eu>
2023-05-03 17:28:22 +0200

    Push ajusted tower prices

--- src/TDTowers/BasicTower.cpp
+++ src/TDTowers/BasicTower.cpp
@@ -5,5 +5,5 @@
 #include "BasicTower.hpp"
 
 BasicTower::BasicTower(Game *gameInstance, int cellSize, SFMLTowerLoader &sfmlTowerLoader, SFMLMissileLoader &sfmlMissileLoader, sf::RenderWindow &window): Tower(gameInstance, 2, cellSize, sfmlTowerLoader, sfmlMissileLoader, window, "BasicTower",
-                                                                                                  {20, 2, 4}, {100, 200, 400}, 7, 0.7, 5, false){
+                                                                                                  {20, 2, 4}, {200, 350, 500}, 7, 0.7, 5, false){
 }
--- src/TDTowers/SplashTower.cpp
+++ src/TDTowers/SplashTower.cpp
@@ -1,7 +1,7 @@
 #include "SplashTower.hpp"
 
 SplashTower::SplashTower(Game *gameInstance, int cellSize, SFMLTowerLoader &sfmlTowerLoader, SFMLMissileLoader &sfmlMissileLoader, sf::RenderWindow &window): Tower(gameInstance, 3, cellSize, sfmlTowerLoader, sfmlMissileLoader, window, "SplashTower",
-                                                                                                                                                                              {7, 13, 20}, {480, 600, 750}, 10, 2.5, 0, false){
+                                                                                                                                                                              {13, 20, 30}, {650, 800, 1000}, 10, 2.5, 0, false){
 }
 
 /*void SplashTower::fire(TDUnit *target){
