eac1dc6fb35beaa596470f50b36435bd974e6fb1
aeppling <adrien.eppling@epitech.eu>
2023-05-03 17:46:22 +0200

    Adjusted tower size
    better sizes

--- src/TDTowers/SlowTower.cpp
+++ src/TDTowers/SlowTower.cpp
@@ -1,7 +1,7 @@
 #include "SlowTower.hpp"
 
 
-SlowTower::SlowTower(Game *gameInstance, int cellSize, SFMLTowerLoader &sfmlTowerLoader, SFMLMissileLoader &sfmlMissileLoader, sf::RenderWindow &window): Tower(gameInstance, 3, cellSize, sfmlTowerLoader, sfmlMissileLoader, window, "SlowTower",
+SlowTower::SlowTower(Game *gameInstance, int cellSize, SFMLTowerLoader &sfmlTowerLoader, SFMLMissileLoader &sfmlMissileLoader, sf::RenderWindow &window): Tower(gameInstance, 2, cellSize, sfmlTowerLoader, sfmlMissileLoader, window, "SlowTower",
                                                                                                                                                                       {8, 12, 16}, {400, 550, 700}, 8, 1.5, 4, false){
 }
 
